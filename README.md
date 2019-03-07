37cc583e383392bc03559a8b48a850060b5ae67d
Victor Olaru <olaruvictor.ov@gmail.com>
2019-03-06 16:27:18 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Project Description
+
+This is a project as part of the robotics course offered by Udacity. It is about ROS and Gazebo usage for the robotics simulation environments.
+
+## Summary of Tasks
+In this project, two ROS packages are created: the drive_bot and the ball_chaser. Here are the caracteristics of the designed robot and its environment:
+
+* drive_bot:
+
+	* A ROS package, named *my_robot* to hold the robot, a white ball, and the world.
+	* Design a differential drive robot with the Unified Robot Description Format with two sensors: a lidar and a camera. Add Gazebo plugins for the robot’s differential drive, lidar, and camera.
+	* The robot is housed inside a world representing an office.
+	* A white-colored ball is added to the Gazebo world.
+	* The world.launch file launches the world with the white-colored ball and the robot.
+
+* ball_chaser:
+
+	* A ball_chaser ROS package to hold your C++ nodes.
+	* A drive_bot C++ node that will provide a ball_chaser/command_robot service to drive the robot by controlling its linear x and angular z velocities.
+	* A process_image C++ node that reads the robot’s camera image and analyzes it to determine the presence and position of a white ball.
+	* The ball_chaser.launch should run both the drive_bot and the process_image nodes.
+
+## Directory structure
+```
+    src                                # Go Chase It Project
+    ├── my_robot                       # my_robot package
+    │   ├── launch                     # launch folder for launch files
+    │   │   ├── robot_description.launch
+    │   │   ├── world.launch
+    │   ├── meshes                     # meshes folder for sensors
+    │   │   ├── hokuyo.dae
+    │   ├── urdf                       # urdf folder for xarco files
+    │   │   ├── my_robot.gazebo
+    │   │   ├── my_robot.xacro
+    │   ├── world                      # world folder for world files
+    │   │   ├── <yourworld>.world
+    │   ├── CMakeLists.txt             # compiler instructions
+    │   ├── package.xml                # package info
+    ├── ball_chaser                    # ball_chaser package
+    │   ├── launch                     # launch folder for launch files
+    │   │   ├── ball_chaser.launch
+    │   ├── src                        # source folder for C++ scripts
+    │   │   ├── drive_bot.cpp
+    │   │   ├── process_images.cpp
+    │   ├── srv                        # service folder for ROS services
+    │   │   ├── DriveToTarget.srv
+    │   ├── CMakeLists.txt             # compiler instructions
+    │   ├── package.xml                # package info
+```
---
+++ ball_chaser/src/process_image.cpp
@@ -0,0 +1,86 @@
+#include "ros/ros.h"
+#include "DriveToTarget.h"
+#include <sensor_msgs/Image.h>
+
+// Define a global client that can request services
+ros::ServiceClient client;
+
+// This function calls the command_robot service to drive the robot in the specified direction
+void drive_robot(float lin_x, float ang_z)
+{
+    ROS_INFO_STREAM("Moving the rover toward the ball.");
+
+    // Request linear and angular velocities.
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    // Call the DriveToTarget service and pass the requested velocities.
+    if (!client.call(srv))
+        ROS_ERROR("Failed to call service DriveToTarget");
+}
+
+// This callback function continuously executes and reads the image data
+void process_image_callback(const sensor_msgs::Image img)
+{
+    int WHITE_PIXEL = 255;
+    int STEP = 2;
+    int LEFT_CENTER_LIMIT = (int)(img.step / 3) ;
+    int RIGHT_CENTER_LIMIT = (int)(img.step / 3) * 2;
+    bool isBallFound = false;
+
+    // Identify if the ball falls in the left, mid, or right side of the image
+    for(int i = 0; i < img.height; i += STEP)
+    {
+        for(int j = 0; j < img.step; j += STEP)
+        {
+            if(static_cast<int>(img.data[(i * img.step) + j]) == WHITE_PIXEL)
+            {
+                if(j < LEFT_CENTER_LIMIT)
+                {
+                    drive_robot(0.0, 0.05 );
+                }
+                else if(j > RIGHT_CENTER_LIMIT)
+                {
+                    drive_robot(0.0, -0.05);
+                }
+                else
+                {
+                    drive_robot(0.05, 0.0);
+                }
+
+                isBallFound = true;
+                break;
+            }
+        }
+
+        if(isBallFound)
+        {
+            break;
+        }
+    }
+
+    // Stop the robot, the ball was not been found.
+    if(!isBallFound)
+    {
+        drive_robot(0.0, 0.0);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    // Initialize the process_image node and create a handle to it
+    ros::init(argc, argv, "process_image");
+    ros::NodeHandle n;
+
+    // Define a client service capable of requesting services from command_robot
+    client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
+
+    // Subscribe to /camera/rgb/image_raw topic to read the image data inside the process_image_callback function
+    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
+
+    // Handle ROS communication events
+    ros::spin();
+
+    return 0;
+}
